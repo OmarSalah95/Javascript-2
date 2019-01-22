9c8d0abe8c19eeed344db238bd21bafa04463d7a
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-22 15:17:52 -0800

    MVP is now done. Pushing onto stretch.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -45,6 +45,7 @@ function multiplyNums(x, y, cb) {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  
 }
 
 /* STRETCH PROBLEM */
